4053484ff5a71854c6aa9c846109bf8eeb6b2b3a
codewiz <c.chaiwat.ch@gmail.com>
2021-06-11 22:09:39 +0700

    add: path parameters

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
 	return { "message": "Hello World" }
+
+@app.get("/books/{book_id}")
+async def read_books(book_id: int):
+	return { "book_id": book_id }
