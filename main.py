5900edd43db0ee3e6993f1d3517702be45972367
codewiz <c.chaiwat.ch@gmail.com>
2021-06-11 22:15:46 +0700

    add: query parameters

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_books_db = [
+	{ "book_name": "Mindset" },
+	{ "book_name": "อิคิไก ความหมายของการมีชีวิตอยู่" },
+	{ "book_name": "เลิกเป็นคนดี แล้วชีวิตจะมีความสุข" },
+]
+
 
 @app.get("/")
 async def root():
@@ -10,3 +16,9 @@ async def root():
 @app.get("/books/{book_id}")
 async def read_books(book_id: int):
 	return { "book_id": book_id }
+
+
+@app.get("/books")
+async def show_all_books(skip: int = 0, limit: int = 10):
+	return fake_books_db[skip: skip + limit]
+	
